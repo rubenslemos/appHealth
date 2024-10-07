30f9ec37ffcedfec4a9690f7c03e59edb6bb66f1
Rubens <rubenslemos@gmail.com>
2024-10-07 20:47:28 -0300

    rotas de empresas

--- index.js
+++ index.js
@@ -36,6 +36,34 @@ app.get('/api/users', async (req, res) => {
 
     res.status(200).json(data);
 });
+
+app.post('/api/companies', async (req, res) => {
+    const { name, isMaster } = req.body;
+
+    // Insere uma nova empresa na tabela 'companies'
+    const { data, error } = await supabase
+        .from('companies') // Nome da tabela
+        .insert([{ name, isMaster }]);
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+
+    res.status(201).json(data);
+});
+
+app.get('/api/companies', async (req, res) => {
+    const { data, error } = await supabase
+        .from('companies')
+        .select('*'); // Seleciona todas as empresas
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+
+    res.status(200).json(data);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
